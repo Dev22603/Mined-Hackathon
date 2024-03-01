76d3b988dda7ec69caf05a31cca36bc727bf7372
Dev Bachani <dev22603@gmail.com>
2024-03-01 11:02:44 +0530

    commit 2

--- TXT.py
+++ TXT.py
@@ -2,8 +2,14 @@ import io
 import pytesseract
 from pdf2image import convert_from_path
 from docx import Document
+import docx2txt
 from bs4 import BeautifulSoup
+import aspose.words as aw
 
+
+
+
+pytesseract.pytesseract.tesseract_cmd="C:\Program Files\Tesseract-OCR\\tesseract.exe"
 class DocumentConverter:
     def extract_text_from_pdf(self, pdf_path):
         pages = convert_from_path(pdf_path, 500)
@@ -38,13 +44,31 @@ class DocumentConverter:
         text_data = extract_text_recursive(soup)
         return text_data
 
+
+    def extract_text_from_doc(self, doc_path):
+        doc = aw.Document(doc_path)
+    
+        # Initialize an empty string to store the text
+        text = ""
+        
+        # Iterate over all paragraphs in the document and extract text
+        for para in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True):
+            text += para.get_text() + "\n"
+        
+        return text
+    def abc(self,b):
+        print("a",b)
+
 # Example usage:
 converter = DocumentConverter()
-text_from_pdf = converter.extract_text_from_pdf('21bce008.pdf')
-print(text_from_pdf)
+# text_from_pdf = converter.extract_text_from_pdf("D:\Mined 24\Resume\Resume(1).pdf")
+# print(text_from_pdf)
 
-text_from_docx = converter.extract_text_from_docx('/content/Sindhu SDE Resume.docx')
-print(text_from_docx)
+# text_from_docx = converter.convert_doc_to_txt("D:\Mined 24\Resume\Srikanth_SrDataEngineer - Copy.doc")
+# print(text_from_docx)
 
-text_from_html = converter.extract_text_from_html('practical2a.html')
-print(text_from_html)
+# text_from_html = converter.extract_text_from_html('practical2a.html')
+# print(text_from_html)
+
+# print(converter.extract_text_from_pdf("D:\Mined 24\Resume\Resume(1).pdf"))
+# converter.abc(22)
